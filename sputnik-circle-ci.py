f263be564dafcf87a80beddfe4e3620d5b93c156
Marcin Smialek <msm@touk.pl>
2016-02-23 16:39:37 +0100

    cokolwiek

--- sputnik-circle-ci.py
+++ sputnik-circle-ci.py
@@ -27,7 +27,7 @@ def get_env(single_env):
 def is_set_every_required_env_variable():
     logging.info("Check required env variables")
     # required_vars = ["CI", "TRAVIS", "TRAVIS_PULL_REQUEST", "TRAVIS_REPO_SLUG"]
-    required_vars = ["CI", "CIRCLE_PROJECT_USERNAME", "CIRCLE_PROJECT_REPONAME", "CIRCLE_PULL_REQUEST"]
+    required_vars = ["CI", "CIRCLECI", "CIRCLE_PROJECT_USERNAME", "CIRCLE_PROJECT_REPONAME", "CI_PULL_REQUEST"]
     for env_var in required_vars:
         if get_env(env_var) is None:
             logging.error("Env variable " + env_var + " is required to run sputnik")
@@ -37,11 +37,11 @@ def is_set_every_required_env_variable():
 
 def is_travis_ci():
     # if get_env("CI") == 'true' and get_env("TRAVIS") == 'true' and get_env("TRAVIS_PULL_REQUEST") != "false":
-    if get_env("CI") == 'true':
+    if get_env("CI") == 'true' and get_env("CIRCLECI") == 'true' and get_env("CI_PULL_REQUEST") != "false":
         return True
     else:
         logging.warn("Stop travis continuous integration. Check evn variables CI: " + get_env("CI")
-                     + ", TRAVIS: " + get_env("TRAVIS") + ", TRAVIS_PULL_REQUEST: " + get_env("TRAVIS_PULL_REQUEST"))
+                     + ", CIRCLECI: " + get_env("CIRCLECI") + ", CI_PULL_REQUEST: " + get_env("CI_PULL_REQUEST"))
         return False
 
 
@@ -70,7 +70,7 @@ def download_files_and_run_sputnik():
         sputnik_jar_url = "http://repo1.maven.org/maven2/pl/touk/sputnik/1.6.0/sputnik-1.6.0-all.jar"
         download_file(sputnik_jar_url, "sputnik.jar")
 
-        subprocess.call(['java', '-jar', 'sputnik.jar', '--conf', 'sputnik.properties', '--pullRequestId', get_env("CIRCLE_PULL_REQUEST")])
+        subprocess.call(['java', '-jar', 'sputnik.jar', '--conf', 'sputnik.properties', '--pullRequestId', get_env("CI_PULL_REQUEST")])
 
 
 def sputnik_ci():
