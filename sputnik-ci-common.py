f7f2db3d132b4b57c7f44cb72f4cbd8995160c47
Marcin Smialek <msm@touk.pl>
2016-03-11 12:12:51 +0100

    test ci script

--- sputnik-ci-common.py
+++ sputnik-ci-common.py
@@ -115,7 +115,7 @@ def download_files_and_run_sputnik(ci_variables):
         logging.debug('Sputnik jar url: ' + sputnik_jar_url)
         download_file(sputnik_jar_url, "sputnik.jar")
 
-        logging.debug('Sputnik env variables: ' + ci_variables)
+        logging.debug('Sputnik env variables: ' + str(ci_variables))
         subprocess.call(['java', '-jar', 'sputnik.jar', '--conf', 'sputnik.properties', '--pullRequestId',
                          ci_variables.pull_request_number, '--apiKey', ci_variables.api_key])
 
